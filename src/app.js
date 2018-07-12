438bd8ff585428bbc6b5519cd512a85917fe6d32
hayato-yamashita <h-yamashita@tes.co.jp>
2018-07-12 09:23:49 +0900

    first commit.

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import {inject, PLATFORM} from 'aurelia-framework';
+import {WebAPI} from './web-api';
+
+@inject(WebAPI)
+export class App {
+  constructor(api) {
+    this.api = api;
+  }
+
+  configureRouter(config, router) {
+    config.title = 'Contacts';
+    config.map([
+      { route: '',              moduleId: PLATFORM.moduleName('no-selection'),   title: 'Select'},
+      { route: 'contacts/:id',  moduleId: PLATFORM.moduleName('contact-detail'), name: 'contacts' }
+    ]);
+
+    this.router = router;
+  }
+}
